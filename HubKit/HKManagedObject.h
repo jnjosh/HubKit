f1c846324cdac5705052231dcfae4e389c36a627
Rhys Powell <rhys@rpowell.me>
2013-01-28 17:51:38 +1100

    Started documenting HKManagedObject

--- HubKit/HKManagedObject.h
+++ HubKit/HKManagedObject.h
@@ -22,30 +22,122 @@
 
 #import <CoreData/CoreData.h>
 
+/**
+ `HKManagedObject` acts as a base class for all the managed objects in HubKit. It
+ provides some convenience methods on top of Core Data, notably:
+ 
+ - Simplified access to a global `NSManagedObjectContext`, `NSManagedObjectModel`,
+   and `NSPersistentStoreCoordinator`
+ - A more straightforward method for initialising new objects through the
+   `initWithContext:` method.
+ - A `save` method on all managed objects
+ 
+ @see NSRemoteManagedObjectContext
+ */
+
 @interface HKManagedObject : NSManagedObject
 
-// Accessing the Main Context
+/** @name Getting the Managed Object Context */
+
+/** The main `NSManagedObjectContext` used for this class of object 
+ 
+ @return an `NSManagedObjectContext
+ */
 + (NSManagedObjectContext *)mainContext;
+
+/** Whether this class currently has a main context
+ 
+ @return a `BOOL` indicating whether this class currently has a main context
+ */
 + (BOOL)hasMainContext;
 
-// Configuring the Persistent Store
+/** @name Configuring the Persistent Store */
+
+/** A shared persistent store coordinator for this class
+ 
+ @return an `NSPersistentStoreCoordinator`
+ */
 + (NSPersistentStoreCoordinator *)persistentStoreCoordinator;
+
+/** Options to be used for this class's persistent store
+ 
+ If these options haven't been set by the time the persistent store is initialised, 
+ a set of defaults will be used.
+ 
+ @return an `NSDictionary` of options for this persistent store
+ */
 + (NSDictionary *)persistentStoreOptions;
+
+/** Set options for the persistent store */
 + (void)setPersistentStoreOptions:(NSDictionary *)options;
+
+/** The managed object model
+ 
+ @return an `NSManagedObjectModel`
+ */
 + (NSManagedObjectModel *)managedObjectModel;
+
+/** Set the managed object model */
 + (void)setManagedObjectModel:(NSManagedObjectModel *)model;
+
+/** The location of the persistent store
+ 
+ @return an `NSURL` indicating the location of the store on disk
+ */
 + (NSURL *)persistentStoreURL;
+
+/** Set the persistent store's location */
 + (void)setPersistentStoreURL:(NSURL *)url;
 
-// Getting Entity Information
+/** @name  Getting Entity Information */
+
+/** The entity's name in the data model
+ 
+ It's intended that subclasses override this if they use a class prefix,
+ otherwise it'll use the class's name.
+ 
+ @return an `NSString` corresponding to the entity's name in the data model
+ */
 + (NSString *)entityName;
+
+/** The `NSEntityDescription` for this class, using the default context
+ 
+ @return The `NSEntityDescription` for this class
+ */
 + (NSEntityDescription *)entity;
+
+/** The `NSEntityDescription` for this class, using a custom context
+ 
+ @param context the `NSManagedObjectContext` to be used
+ 
+ @return The `NSEntityDescription` for this class
+ */
 + (NSEntityDescription *)entityWithContext:(NSManagedObjectContext *)context;
+
+/** Default sort descriptors for use with this class 
+ 
+ It's intended that subclasses override this to provide sensible sorting rules.
+ 
+ @return an array of `NSSortDescriptor` objects
+ */
 + (NSArray *)defaultSortDescriptors;
 
-// Initializing
+/** @name Creating New Objects */
+
+/** Init a new object with a given context
+ 
+ This method is the class's designated initialiser. Calling `init` on this class
+ simply calls this method with the main context.
+ 
+ @param context the `NSManagedObjectContext` to be used.
+ 
+ @return a newly instantiated object
+ */
 - (instancetype)initWithContext:(NSManagedObjectContext *)context;
 
+/** @name Saving Objects */
+
+/** Save this object's managed object context */
 - (void)save;
 
 @end
